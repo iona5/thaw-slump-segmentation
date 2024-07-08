e5642aa78548d58a3596a603f5643fdb465539e1
Jonas Küpper <jonas.kuepper@awi.de>
2024-07-08 08:12:21 +0200

    CLI/preprocess:  more CLI help

--- src/thaw_slump_segmentation/scripts/setup_raw_data.py
+++ src/thaw_slump_segmentation/scripts/setup_raw_data.py
@@ -43,24 +43,36 @@ STATUS = {0: 'failed', 1: 'success', 2: 'skipped'}
 SUCCESS_STATES = ['rename', 'label', 'ndvi', 'tcvis', 'rel_dem', 'slope', 'mask', 'move']
 
 
-preprocess_app = typer.Typer()
+preprocess_app = typer.Typer(no_args_is_help=True)
 
 @preprocess_app.command()
 def preprocess_directory(
-    image_dir  : Annotated[Path, typer.Option('--image_dir', help='The directory where SR and optionally the mask reside')], 
-    data_dir   : Annotated[Path, typer.Option('--data_dir', help='The directory where to write the masked image to if it exists')], 
-    aux_dir    : Annotated[Path, typer.Option('--aux_dir', help='The directory where aux data, specifically the ArcticDEM virtual raster files reside')], 
-    backup_dir : Annotated[Path, typer.Option('--backup_dir', help='output directory')], 
-    log_path   : Annotated[str, typer.Option('--log_path', help='Logging path')], 
+    image_dir  : Annotated[Path, typer.Option('--image_dir', 
+                            help='The directory where SR and optionally the mask reside',
+                            rich_help_panel="Input Directories")], 
+    data_dir   : Annotated[Path, typer.Option('--data_dir', 
+                            help='The directory where to write the masked image to if it exists',
+                            rich_help_panel="Output")], 
+    aux_dir    : Annotated[Path, typer.Option('--aux_dir', 
+                            help='The directory where aux data, specifically the ArcticDEM virtual raster files reside',
+                            rich_help_panel="Input Directories")], 
+    backup_dir : Annotated[Path, typer.Option('--backup_dir', 
+                            help='output directory',
+                            rich_help_panel="Output")], 
+    log_path   : Annotated[str, typer.Option('--log_path', 
+                            help='Logging path',
+                            rich_help_panel="Output")], 
     gdal_bin   : Annotated[str, typer.Option('--gdal_bin', 
-                                                    help='Path to GDAL binaries (like gdalwarp)',
-                                                    envvar='GDAL_BIN')], 
+                            help='Path to GDAL binaries (like gdalwarp)',
+                            rich_help_panel="GDAL",
+                            envvar='GDAL_BIN')], 
     gdal_path  : Annotated[str, typer.Option('--gdal_path',
-                                                    help='Path to GDAL scripts (like gdal_retile.py)',
-                                                    envvar="GDAL_PATH")] 
-                                                    = None, 
+                            help='Path to GDAL scripts (like gdal_retile.py)',
+                            rich_help_panel="GDAL",
+                            envvar="GDAL_PATH")] 
+                            = None, 
     label_required: Annotated[bool, typer.Option('--label_required/--no_labels', 
-                    help='if labels should be searched for and baked into tiles (only needed for training)')] 
+                    help='if label data should be searched for and baked into tiles (only needed for training)')] 
                     = True
     ):
     # Mock old args object
